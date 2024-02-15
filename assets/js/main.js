6ac0dae637e8ab8617eec6dde1b0c1067eed8b16
alexis velluet <alexis.velluet@outlook.com>
2024-02-15 14:20:16 +0100

    responsive layout,scss and js done

--- assets/js/main.js
+++ assets/js/main.js
@@ -209,15 +209,36 @@
 					}).trigger('resize.ie-intro-fix');
 				}
 
-			// Hide intro on scroll (> small).
-				breakpoints.on('>small', function() {
+
+// Hide intro on scroll (>medium).
+        breakpoints.on('>medium', function() {
+
+          $main.unscrollex();
+
+          $main.scrollex({
+            mode: 'bottom',
+            top: '40vh',
+            bottom: '-50vh',
+            enter: function() {
+              $intro.addClass('hidden');
+            },
+            leave: function() {
+              $intro.removeClass('hidden');
+            }
+          });
+        });
+
+
+
+			// Hide intro on scroll (<= medium).
+				breakpoints.on('<=medium', function() {
 
 					$main.unscrollex();
 
 					$main.scrollex({
 						mode: 'bottom',
-						top: '25vh',
-						bottom: '-50vh',
+						top: '50vh',
+						bottom: '-40vh',
 						enter: function() {
 							$intro.addClass('hidden');
 						},
@@ -228,6 +249,7 @@
 
 				});
 
+
 			// Hide intro on scroll (<= small).
 				breakpoints.on('<=small', function() {
 
@@ -235,8 +257,8 @@
 
 					$main.scrollex({
 						mode: 'middle',
-						top: '15vh',
-						bottom: '-15vh',
+						top: '20vh',
+						bottom: '-70vh',
 						enter: function() {
 							$intro.addClass('hidden');
 						},
@@ -250,4 +272,3 @@
 		}
 
 })(jQuery);
-
