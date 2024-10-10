296afd559e8737dc5916636bc98802ccf8a9db5a
alexis velluet <alexis.velluet@outlook.com>
2024-10-10 15:32:38 +0200

    redirection relative dossier et js avec ./

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,21 @@ import {defineConfig} from 'vite'
 
 
 export default defineConfig({
-	plugins: []	
+  base: './', // Indique d'utiliser des chemins relatifs
+	plugins: [],
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        about: 'about.html',
+        bff: 'project-bff.html',
+        escape: 'project-escape.html',
+        butterfly:  'project-butterfly.html',
+        converter:  'project-converter.html',
+        password:  'project-password.html',
+        portal:  'project-portal.html',
+        train:  'project-train.html',
+      }
+    }
+  },
 })
